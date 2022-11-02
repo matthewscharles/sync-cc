f1adb0b5384525f9f32f16012f0569c9f5588a5d
Charles Matthews <charles0matthews@gmail.com>
2022-11-02 15:00:30 -0400

    temporary workaround for issue with scope/this in httprequest

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # captions-sbv
 Sync captions to an external video source and place in an HTML container.
+
+Caption test video: https://youtu.be/Tbq-pNsVsjM
---
+++ test_src/test.js
@@ -0,0 +1,5 @@
+// const CM = require('cm-toolbox');
+// window.cm = new CM();
+// const fs = require('fs');
+const CaptionsSBV = require('../index.js')
+window.captionsSBV = new CaptionsSBV(document.querySelector('#captions_div'))
