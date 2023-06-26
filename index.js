bad5a232804fe6ce577bad638d18502cd8b9a553
Charles Matthews <charles0matthews@gmail.com>
2023-06-26 15:27:37 -0400

    add an srt -> sbv converter

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 /**
+ * 
+ * @param {string} srtString 
+ * @returns {string} captions in .sbv format
+ */
+
+function srtToSbv(srtString) {
+    // Split the string into individual captions
+    const captions = srtString.trim().split(/\n\s*\n/);
+  
+    // Convert each caption to the .sbv format, removing the arrow ("-->") character
+    const sbvCaptions = captions.map((caption, index) => {
+      // Split the caption into its individual lines
+      const lines = caption.trim().split(/\n/);
+  
+      // Extract the start and end times from the first line
+      const [startTime, endTime] = lines[1].split(' --> ');
+  
+      // Format the start and end times
+      const times = [startTime.replace(',', '.'), endTime.replace(',', '.')];
+
+      // Join the remaining lines to form the caption text
+      const text = lines.slice(2).join('\n');
+  
+      // Combine the formatted start and end times and the caption text to form a single string
+      const output = `${index + 1}\n${times[0]},${times[1]}\n${text}\n`;
+
+      return output;
+    });
+  
+    // Join the .sbv captions into a single string
+    return sbvCaptions.join('');
+  }
+
+
+/**
  * Sync captions to an external video source and place in an HTML container.
  * @class
  * @property {Object}   script
@@ -12,7 +47,8 @@ const SyncCC = function(container){
     this.timeline   =   [];
     this.filename   =   '';
     this.container  =   container;
-    
+    // quick fix for testing
+    window.srtToSbv = srtToSbv;
 }
 
 SyncCC.prototype = {}
