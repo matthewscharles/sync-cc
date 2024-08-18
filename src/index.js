57c9e45e263700e64ad6f355f579b03e9e89de1b
Charles Matthews <charles0matthews@gmail.com>
2024-08-18 12:20:16 -0400

    switch to css classes instead of styles, fix timeToMs

--- src/index.js
+++ src/index.js
@@ -96,12 +96,14 @@ SyncCC.prototype.updateCaptions = function(time, display=true, enabled = true){
         } else {
 
         }
-        // todo: look into adding/removing classes instead of setting opacity -- more flexibility
-        this.container.style.opacity = enabled ? 1 : 0;
+        
+        // this.container.style.opacity = enabled ? 1 : 0;
+        this.container.classList.toggle('hidden', !enabled);
 
     } else {
         this.container.innerHTML = ""
-        this.container.style.opacity = 0;
+        this.container.classList.toggle('hidden', true);
+        // this.container.style.opacity = 0;
     } 
 }
 
@@ -128,7 +130,7 @@ SyncCC.prototype.updateCaptions = function(time, display=true, enabled = true){
  * @returns {number} time in seconds, or blank string if blank ?!
  */
 
-SyncCC.prototype.convertTimetoMs = function(timeString){
+function convertTimeToMs(timeString){
     if(timeString=="" || typeof timeString=="undefined") {
         // input is blank, return blank string
         console.log('nothing to convert...');
@@ -152,25 +154,9 @@ SyncCC.prototype.convertTimetoMs = function(timeString){
  */
 
 SyncCC.prototype.splitCaptions = function(script){
-    // something is going wrong with scope/this around the HTTPrequest, so duplicating the convertTimeToMs function here
-    function convertTimeToMs(timeString){
-        if(timeString=="" || typeof timeString=="undefined") {
-            // input is blank, return blank string
-            return "";
-        }
-        let timeSplit=timeString.split(':')
-        let time = {
-            hours:      parseInt(timeSplit[0]),
-            minutes:    parseInt(timeSplit[1]),
-            seconds:    parseInt(timeSplit[2].split('.')[0]),
-            ms:         parseInt(timeSplit[2].split('.')[1])
-        }
-        let totalMs = time.ms + (time.seconds*1000) + (time.minutes*60*1000) + (time.hours*60*60*1000)
-        return totalMs/1000;
-    }
-
-
-    let output = []
+    let output = [];
+    
+    
     script.forEach((event,i)=>{
         if(event=="")return;
             let lineTest={
@@ -192,8 +178,6 @@ SyncCC.prototype.splitCaptions = function(script){
  */
 
  SyncCC.prototype.requestListener = function requestListener() {
-    // troubleshooting
-    console.log('requestListener', this.responseText, this.captionTarget);
     if(!this.captionTarget) {window.textOutput = this.responseText; return false};
     this.captionTarget.script.events = this.responseText.split('\n\n')
     this.captionTarget.timeline = this.captionTarget.splitCaptions(this.captionTarget.script.events, this.captionTarget);
@@ -221,6 +205,10 @@ SyncCC.prototype.loadCaptions = function(filename){
     request.onerror = this.requestError;
     request.open('get', `./${this.filename}`, true);
     request.send();
+}
+
+SyncCC.prototype.setInterval = function(player, interval=100){
+    setInterval(()=>{this.updateCaptions(player.getCurrentTime())},interval).bind(this);
 }
 
 export { SyncCC };
